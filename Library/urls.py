9bbf2fae90a4c4026a0999af6052fe2a2589bb9b
CemHarput <cemharput@gmail.com>
2022-04-07 16:22:33 +0300

    Auth Completed

--- Library/urls.py
+++ Library/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_Page, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
     path('update/<str:pk>', views.update),
     path('view/', views.view),
     path('delete/', views.delete),
-    path('', views.home),
+    path('', views.home,name='home'),
 ]
--- Library/views.py
+++ Library/views.py
@@ -1,14 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from Library.forms import CreateUserForm
 
 
-def login(request):
-    return render(request, 'login.html')
+def login_Page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'login.html', context)
 
 
 def register(request):
@@ -18,9 +31,16 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Registered Successfully')
+            return redirect('login')
     context = {'form': form}
     return render(request, 'register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return render(request, 'login.html')
 
 
 def update(request, pk):
